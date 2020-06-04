094425e95ca61f66234652ce0697ff479f314f2d
Felipe Secato <felipe.secato@gmail.com>
2020-06-04 15:14:14 -0300

    Integrate notification service with SQS

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'us-east-1' });
 
 async function hello(event, context) {
+  const record = event.Records[0];
+  console.log('processing record: ', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: 'felipe.secato@gmail.com',
     Destination: {
-      ToAddresses: ['felipe.secato@gmail.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from AWS Lambda',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       },
     },
   };
